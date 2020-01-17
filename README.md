2ff2eb34c297649d33a93d7d7abcdbeada4e8441
Mathieu <github@kumy.net>
2020-01-18 00:51:09 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # Roomba980-Wifi
 Configure irobot wifi series using command line
+
+I finally got there!
+
+# Steps
+## Push a password
+1. Reset to robot (press Home+Spot+Clean until all lights up)
+2. Start Soft AP (press Home+Spot) until sound + green wifi led blink - Eventually try again if the error bip sounds
+1. The password is pushed to the robot using an MQTT Authentication Exchange packet
+    `echo -n "f023efcc3b29003a313a313537393139353338363a386678376e597156744b67574a39744f" | xxd -r -p | openssl s_client -CAfile robot-ca.pem  -connect 192.168.10.1 -quiet -noservername`. This output the password back if set correctly.
+
+**packet format:**
+
+Value | Description
+----- | ----------
+`0xf0` | MQTT Authentication Exchange packet fixed header
+`0x23` | The size of the remaining Bytes after this (35 in decimal)
+`0xefcc3b2900` | Voodoo packet :smile_cat:
+`:1:1579195386:8fx7nYqVtKgWJ9tO` | Format: `:1:timestamp:16 alpha-decimal chars`
+
+Note: I have not played using different password format, so use with caution.
+
+## Provision wifi settings
+Connect using mqtt protocol v3.1.1 and send commands via `json` messages from the table bellow on the right topic and **disconnect after**. (I use only 6, 9, 10, 11, 12, it seems to be sufficient for a basic configuration). Having a little pause between commands seems to help the process a bit. Once sent, the SoftAP should turn off and the wifi led should turn fixed white.
+
+&nbsp; | Topic | payload | Comment
+------- | ------- | ---------- | --------------
+1 | delta | { "state" : { "timezone" : "Europe/Paris" } } | makes the robot bip once of two
+2 | wifictl | { "state" : { "sdiscUrl" : "https://disc-prod.iot.irobotapi.com/v1/robot/discover?robot_id=0000000000000000&country_code=FR&sku=R966040" } } | I did not checked what is this api…
+3 | wifictl | { "state" : { "ntphosts" : "0.irobot.pool.ntp.org 1.irobot.pool.ntp.org 2.irobot.pool.ntp.org 3.irobot.pool.ntp.org" } }
+4 | delta | { "state" : {"country" : "FR"} }
+5 | delta | { "state" : { "cloudEnv" : "prod" } }
+6 | wifictl | {"state": {"wlcfg": {"pass": "wifisecretpasssword", "sec": 7, "ssid": "575757"}}} | `pass` as clear text, `ssid` as octal (mine is `WWW` here), `sec`???
+7 | wifictl | { "state" : { "utctime" : 1579291795 } } | timestamp
+8 | wifictl | { "state" : { "localtimeoffset" : 60 } }
+9 | wifictl | { "state" : { "chkssid" : true } }
+10 | wifictl | { "state" : { "wactivate" : true } }
+11 | wifictl | { "state" : { "get" : "netinfo" } }
+12 | wifictl | { "state" : { "uap" : false } }
+
+## Python script
+
+There is a proof of concept in this repo.
+
+```
+$ ./initial-config.py
+received data: hex: b'f023', length: 2
+received data: hex: b'efcc3b29003a313a313537393139353338363a386678376e597156744b67574a39744f', length: 35
+received data: hex: b'f023efcc3b29003a313a313537393139353338363a386678376e597156744b67574a39744f', length: 37
+Sending: delta { "state" : { "timezone" : "Europe/Paris" } }
+Sending: wifictl {"state": {"wlcfg": {"pass": "wifisecretpasssword", "sec": 7, "ssid": "575757"}}}
+Sending: wifictl { "state" : { "chkssid" : true } }
+Sending: wifictl { "state" : { "wactivate" : true } }
+Sending: wifictl { "state" : { "get" : "netinfo" } }
+Sending: wifictl { "state" : { "uap" : false } }
+```
+
+# Thanks
+I want to thanks my wife and my familly ;)
+
+Some code in the prototype copied from https://github.com/NickWaterton/Roomba980-Python
---
+++ initial-config.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+import binascii
+import sys
+import socket
+import ssl
+import struct
+import time
+import paho.mqtt.client as mqtt
+
+# Get Robot CA
+# openssl s_client -showcerts -connect 192.168.10.1 < /dev/null 2>/dev/null|openssl x509 -outform PEM > robot-ca.pem
+
+msgs = [
+    #{'topic':"delta", 'payload':'{ "state" : { "timezone" : "Europe/Paris" } }'},
+    #{'topic':"wifictl", 'payload':'{ "state" : { "sdiscUrl" : "https://disc-prod.iot.irobotapi.com/v1/robot/discover?robot_id=0000000000000000&country_code=FR&sku=R966040" } }'},
+    #{'topic':"wifictl", 'payload':'{ "state" : { "ntphosts" : "0.irobot.pool.ntp.org 1.irobot.pool.ntp.org 2.irobot.pool.ntp.org 3.irobot.pool.ntp.org" } }'},
+    #{'topic':"delta", 'payload':'{ "state" : {"country" : "FR"} }'},
+    #{'topic':"delta", 'payload':'{ "state" : { "cloudEnv" : "prod" } }'},
+    {'topic':"wifictl", 'payload':'{"state": {"wlcfg": {"pass": "wifisecretpasssword", "sec": 7, "ssid": "575757"}}}'},  # ssid as octal ("WWW" here)
+    #{'topic':"wifictl", 'payload':'{ "state" : { "utctime" : 1579291795 } }'},
+    #{'topic':"wifictl", 'payload':'{ "state" : { "localtimeoffset" : 60 } }'},
+    {'topic':"wifictl", 'payload':'{ "state" : { "chkssid" : true } }'},
+    {'topic':"wifictl", 'payload':'{ "state" : { "wactivate" : true } }'},
+    {'topic':"wifictl", 'payload':'{ "state" : { "get" : "netinfo" } }'},
+    {'topic':"wifictl", 'payload':'{ "state" : { "uap" : false } }'},
+]
+
+# voodoo packet?
+MAGIC_PACKET=b'\xef\xcc\x3b\x29\x00'
+
+# format:  :1:timestamp:16 alpha-decimal chars
+PASSWORD=b':1:1579195386:8fx7nYqVtKgWJ9tO'
+
+HOST="192.168.10.1"
+PORT=8883
+
+def provision_password():
+    payload=MAGIC_PACKET+PASSWORD
+    authentication_exchange=b'\xf0'+bytes([len(payload)])+payload
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(10)
+    wrappedSocket = ssl.wrap_socket(sock, ssl_version=ssl.PROTOCOL_TLS, ciphers='DEFAULT@SECLEVEL=1')
+
+    try:
+        wrappedSocket.connect((HOST, PORT))
+    except Exception as e:
+        print("Connection Error %s" % e)
+
+    wrappedSocket.send(authentication_exchange)
+
+    data = b''
+    data_len = len(payload)
+    while True:
+        try:
+            # NOTE data is 0xf0 (mqtt RESERVED) length (0x23 = 35),
+            # 0xefcc3b2900 (magic packet), 0xXXXX... (30 bytes of
+            # password). so 7 bytes, followed by 30 bytes of password
+            # (total of 37)
+            if len(data) >= data_len+2:
+                break
+            data_received = wrappedSocket.recv(1024)
+            print("received data: hex: %s, length: %d" % (binascii.hexlify(data_received), len(data_received)), flush=True)
+        except socket.error as e:
+            print("Socket Error: %s" % e)
+            break
+
+        if len(data_received) == 0:
+            print("socket closed")
+            break
+        else:
+            data += data_received
+            if len(data) >= 2:
+                data_len = struct.unpack("B", data[1:2])[0]
+
+    wrappedSocket.close()
+    print("received data: hex: %s, length: %d" % (binascii.hexlify(data), len(data)))
+
+    if len(data) <= 7:
+        print('Error setting password, receive %d bytes. Follow the '
+              'instructions and try again.' % len(data))
+        sys.exit(1)
+    time.sleep(2)
+
+def provision_wifi():
+    client = mqtt.Client('0000000000000000')
+    client.tls_set(ca_certs='/path/to/robot-ca.pem', cert_reqs=ssl.CERT_NONE)
+    client.tls_insecure_set(True)
+    client.username_pw_set('0000000000000000', PASSWORD)
+    client.tls_insecure_set(True)
+    client.connect("192.168.10.1", 8883, 60)
+    
+    for msg in msgs:
+        print('Sending:', msg['topic'], msg['payload'], flush=True)
+        client.publish(msg['topic'], msg['payload'])
+        time.sleep(1)
+      
+    client.disconnect()
+    
+
+
+def main():
+    provision_password()
+    provision_wifi()
+
+if __name__ == "__main__":
+    main()
